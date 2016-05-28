c60609d36deb9191ceb8bbd88ded41f125d5e2a4
pedrokarut <pedrokarut@hotmail.com>
2016-05-28 14:03:54 -0300

    app

---
+++ app/src/main/java/pedro/ifsp/br/trabalhobimestral/Fase1.java
@@ -0,0 +1,19 @@
+package pedro.ifsp.br.trabalhobimestral;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Fase1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fase1);
+    }
+
+
+    private void ImagemClick(View v)
+    {
+    }
+}
---
+++ app/src/main/java/pedro/ifsp/br/trabalhobimestral/MainActivity.java
@@ -0,0 +1,24 @@
+package pedro.ifsp.br.trabalhobimestral;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void callFase1(View v)
+    {
+
+    }
+
+    private void callInformacoes(View v)
+    {
+
+    }
+}
